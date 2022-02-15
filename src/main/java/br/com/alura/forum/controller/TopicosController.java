b50b4825fd19874e425b3b2fee5599ef4020a1d4
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-02-15 15:20:11 -0300

    Paginacao e ordenacao

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -36,9 +37,9 @@ public class TopicosController {
 	
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,@RequestParam int pagina,@RequestParam int qtd
-								 ) {
+								 ,@RequestParam String ordenacao) {
 
-		Pageable paginacao =  PageRequest.of(pagina,qtd);
+		Pageable paginacao =  PageRequest.of(pagina,qtd, Sort.Direction.ASC, ordenacao);
 
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
