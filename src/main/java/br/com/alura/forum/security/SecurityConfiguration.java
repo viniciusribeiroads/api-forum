2e01b950ea32512648f621e2e9327feb8b3807fc
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-02-16 08:28:00 -0300

    Authentication stateless using token

--- src/main/java/br/com/alura/forum/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/security/SecurityConfiguration.java
@@ -1,14 +1,19 @@
 package br.com.alura.forum.security;
 
+import br.com.alura.forum.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -16,7 +21,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AutenticacaoService autenticacaoService;
+    @Autowired
+    private TokenServices tokenServices;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     //Configurar autenticacao
     @Override
@@ -30,8 +44,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin();
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFIlter(tokenServices, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
     }
 
     //Configuracoes de recursos estaticos(js, css, imagens, etc..)
@@ -39,7 +56,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
     }
 
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123456"));
-    }
+    //public static void main(String[] args) {
+    //    System.out.println(new BCryptPasswordEncoder().encode("123456"));
+    //}
 }
